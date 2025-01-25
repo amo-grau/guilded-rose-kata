b14b88f9cf6952eff2a044f98af994c52866abcb
amo-grau <oscaramog@gmail.com>
2025-01-25 18:03:02 +0100

    extracted increase quality method

--- csharp.xUnit/GildedRose/GildedRose.cs
+++ csharp.xUnit/GildedRose/GildedRose.cs
@@ -18,41 +18,9 @@ public class GildedRose
     {
         foreach(var item in Items)
         {
-            if (Is(item, "Backstage passes to a TAFKAL80ETC concert"))
-            {
-                if (SellInDateLowerThan(6, item))
-                    IncreaseQuality(item, 3);
+            IncreaseQualityForPositiveSellInDate(item);
 
-                else if (SellInDateLowerThan(11, item))
-                    IncreaseQuality(item, 2);
 
-                else 
-                    IncreaseQuality(item, 1);
-            }
-            else if (Is(item, "Aged Brie"))
-            {
-                IncreaseQuality(item, 1);   
-            }
-            else if (Is(item, "Sulfuras, Hand of Ragnaros"))
-            {
-            }
-            else
-            {
-                IncreaseQuality(item, -1); // that's the normal case!!
-            }
-
-            if (Is(item, "Sulfuras, Hand of Ragnaros"))
-            {
-                if (item.Quality != 80)
-                    item.Quality = 80;
-            }
-            else
-            {
-
-                if (item.Quality > 50)
-                    item.Quality = 50;
-            }
-            
             if (!Is(item, "Sulfuras, Hand of Ragnaros"))
                 item.SellIn = item.SellIn - 1; // normal case
 
@@ -84,12 +52,46 @@ public class GildedRose
                 }
             }
 
-            if (!HasPositiveQuality(item))
+            if (Is(item, "Sulfuras, Hand of Ragnaros"))
             {
-                item.Quality = 0;
+                if (item.Quality != 80)
+                    item.Quality = 80;
+            }
+            else
+            {
+                if (item.Quality > 50)
+                    item.Quality = 50;
+                if (!HasPositiveQuality(item))
+                    item.Quality = 0;
             }
         }
 
+    }
+
+    private void IncreaseQualityForPositiveSellInDate(Item item)
+    {
+        if (Is(item, "Backstage passes to a TAFKAL80ETC concert"))
+        {
+            if (SellInDateLowerThan(6, item))
+                IncreaseQuality(item, 3);
+
+            else if (SellInDateLowerThan(11, item))
+                IncreaseQuality(item, 2);
+
+            else
+                IncreaseQuality(item, 1);
+        }
+        else if (Is(item, "Aged Brie"))
+        {
+            IncreaseQuality(item, 1);
+        }
+        else if (Is(item, "Sulfuras, Hand of Ragnaros"))
+        {
+        }
+        else
+        {
+            IncreaseQuality(item, -1); // that's the normal case!!
+        }
     }
 
     private bool Is(Item item, string type){
