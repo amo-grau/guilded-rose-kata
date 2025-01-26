77f2b6944e79c35e76628083eefe8d1212ca1d2b
amo-grau <oscaramog@gmail.com>
2025-01-26 12:13:48 +0100

    encapsulated item in itemHandler

--- csharp.xUnit/GildedRose/GildedRose.cs
+++ csharp.xUnit/GildedRose/GildedRose.cs
@@ -4,6 +4,7 @@ using System.Formats.Asn1;
 using System.Linq;
 using System.Runtime;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace GildedRoseKata;
 
@@ -12,9 +13,11 @@ public class GildedRose
     public GildedRose(IList<Item> Items)
     {
         this.Items = Items.ToList();
+        ItemHandlers = this.Items.Select(item => new ItemHandler(item)).ToList();
     }
  
     List<Item> Items { get; }
+    List<ItemHandler> ItemHandlers { get; }
 
     public void Update()
     {
@@ -24,103 +27,114 @@ public class GildedRose
 
     private void UpdateQuality(Item item)
     {
-        if (SellDatePassed(item))
-            PassedDateUpdate(item);
+        var handler = new ItemHandler(item);
+        if (SellDatePassed(handler.Item))
+            PassedDateUpdate(handler.Item);
 
         else
-            Update(item);
+            Update(handler.Item);
 
-        SetQualityInRange(item);
+        SetQualityInRange(handler.Item);
     }
 
     private void Update(Item item)
     {
-        if (Is(item, "Backstage passes to a TAFKAL80ETC concert"))
-        {
-            if (SellInDateLowerThan(5, item))
-                IncreaseQuality(item, 3);
+        var handler = new ItemHandler(item);
 
-            else if (SellInDateLowerThan(10, item))
-                IncreaseQuality(item, 2);
+        if (Is(handler.Item, "Backstage passes to a TAFKAL80ETC concert"))
+        {
+            if (SellInDateLowerThan(5, handler.Item))
+                IncreaseQuality(handler.Item, 3);
+
+            else if (SellInDateLowerThan(10, handler.Item))
+                IncreaseQuality(handler.Item, 2);
 
             else
-                IncreaseQuality(item, 1);
+                IncreaseQuality(handler.Item, 1);
         }
-        else if (Is(item, "Aged Brie"))
+        else if (Is(handler.Item, "Aged Brie"))
         {
-            IncreaseQuality(item, 1);
+            IncreaseQuality(handler.Item, 1);
         }
-        else if (Is(item, "Sulfuras, Hand of Ragnaros"))
+        else if (Is(handler.Item, "Sulfuras, Hand of Ragnaros"))
         {
         }
         else
         {
-            IncreaseQuality(item, -1); // that's the normal case!!
+            IncreaseQuality(handler.Item, -1); // that's the normal case!!
         }
     }
 
     private void PassedDateUpdate(Item item)
     {
-        if (Is(item, "Aged Brie"))
+        var handler = new ItemHandler(item);
+
+        if (Is(handler.Item, "Aged Brie"))
         {
-            IncreaseQuality(item, 2);
+            IncreaseQuality(handler.Item, 2);
         }
-        else if (Is(item, "Backstage passes to a TAFKAL80ETC concert"))
+        else if (Is(handler.Item, "Backstage passes to a TAFKAL80ETC concert"))
         {
-            IncreaseQuality(item, -item.Quality); // quality = 0 for outdated tickets
+            IncreaseQuality(handler.Item, -item.Quality); // quality = 0 for outdated tickets
         }
-        else if (Is(item, "Sulfuras, Hand of Ragnaros"))
+        else if (Is(handler.Item, "Sulfuras, Hand of Ragnaros"))
         {
             // do nothing
         }
         else
         {
-            IncreaseQuality(item, -2);
+            IncreaseQuality(handler.Item, -2);
         }
     }
 
     private void UpdateSellIn(Item item)
     {
-        if (Is(item, "Sulfuras, Hand of Ragnaros")) 
+        var handler = new ItemHandler(item);
+        if (Is(handler.Item, "Sulfuras, Hand of Ragnaros")) 
         {
             // do nothing
         }
         else
-            item.SellIn = item.SellIn - 1;
+            handler.Item.SellIn = handler.Item.SellIn - 1;
     }
 
     private void SetQualityInRange(Item item)
     {
-        if (Is(item, "Sulfuras, Hand of Ragnaros"))
+        var handler = new ItemHandler(item);
+        if (Is(handler.Item, "Sulfuras, Hand of Ragnaros"))
         {
-            if (item.Quality != 80)
-                item.Quality = 80;
+            if (handler.Item.Quality != 80)
+                handler.Item.Quality = 80;
         }
         else
         {
-            if (item.Quality > 50)
-                item.Quality = 50;
+            if (handler.Item.Quality > 50)
+                handler.Item.Quality = 50;
 
-            if (item.Quality < 0)
-                item.Quality = 0;
+            if (handler.Item.Quality < 0)
+                handler.Item.Quality = 0;
         }
     }
 
     private bool Is(Item item, string type){
-        return item.Name == type;
+        var handler = new ItemHandler(item);
+        return handler.Item.Name == type;
     }
     
     private bool SellDatePassed(Item item){
-        return SellInDateLowerThan(0, item);
+        var handler = new ItemHandler(item);
+        return SellInDateLowerThan(0, handler.Item);
     }
 
     private bool SellInDateLowerThan(int treshold, Item item){
-        return item.SellIn < treshold;
+        var handler = new ItemHandler(item);
+        return handler.Item.SellIn < treshold;
     }
 
     private Item IncreaseQuality(Item item, int amount)
     {
-        var result = item;
+        var handler = new ItemHandler(item);
+        var result = handler.Item;
         result.Quality += amount;
         return result;
     }
---
+++ csharp.xUnit/GildedRose/ItemHandler.cs
@@ -0,0 +1,11 @@
+using GildedRoseKata;
+
+class ItemHandler
+{
+    public ItemHandler(Item item)
+    {
+        Item = item;
+    }
+
+    public Item Item { get; set; }
+}
